d1eccc13060423b0693f46018d132b7c03d3395b
Cheburek <islam2_14@mail.ru>
2023-06-24 19:42:00 +0300

    new files for router-dom n2

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './shared/redux/store.ts';
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./shared/config";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
