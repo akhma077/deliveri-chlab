49dd564c056ed5c695a45c6c7ec53ef426b73fd8
Cheburek <islam2_14@mail.ru>
2023-07-01 23:25:31 +0300

    f13

--- src/pages/Basket/index.tsx
+++ src/pages/Basket/index.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import styles from "./index.module.scss";
 import { BasketContent, BasketHead } from "../../features";
 import testImage from "../../shared/assets/img/b524370421f7498ff97f82f4fe3922ac.jpeg";
+import iconArrowTop from "../../shared/assets/img/iconArrowTop.svg";
+import { Product } from "../../entities";
 
 export const Basket = () => {
     const [modal, setModal] = React.useState<boolean>(false);
@@ -183,8 +185,186 @@ export const Basket = () => {
                 weight: 199,
                 description: "Лучшие суши на свете",
             },
+        ],
+        [
             {
-                id: 314226,
+                id: 12,
+                name: "Суши 4",
+                image: testImage,
+                price: 300,
+                discount: 1,
+                weight: 199,
+                description: "Лучшие суши на свете",
+            },
+        ],
+        [
+            {
+                id: 12,
+                name: "Суши 4",
+                image: testImage,
+                price: 300,
+                discount: 1,
+                weight: 199,
+                description: "Лучшие суши на свете",
+            },
+        ],
+        [
+            {
+                id: 12,
+                name: "Суши 4",
+                image: testImage,
+                price: 300,
+                discount: 1,
+                weight: 199,
+                description: "Лучшие суши на свете",
+            },
+        ],
+        [
+            {
+                id: 12,
+                name: "Суши 4",
+                image: testImage,
+                price: 300,
+                discount: 1,
+                weight: 199,
+                description: "Лучшие суши на свете",
+            },
+        ],
+        [
+            {
+                id: 12,
+                name: "Суши 4",
+                image: testImage,
+                price: 300,
+                discount: 1,
+                weight: 199,
+                description: "Лучшие суши на свете",
+            },
+        ],
+        [
+            {
+                id: 12,
+                name: "Суши 4",
+                image: testImage,
+                price: 300,
+                discount: 1,
+                weight: 199,
+                description: "Лучшие суши на свете",
+            },
+        ],
+        [
+            {
+                id: 12,
+                name: "Суши 4",
+                image: testImage,
+                price: 300,
+                discount: 1,
+                weight: 199,
+                description: "Лучшие суши на свете",
+            },
+        ],
+        [
+            {
+                id: 12,
+                name: "Суши 4",
+                image: testImage,
+                price: 300,
+                discount: 1,
+                weight: 199,
+                description: "Лучшие суши на свете",
+            },
+        ],
+        [
+            {
+                id: 12,
+                name: "Суши 4",
+                image: testImage,
+                price: 300,
+                discount: 1,
+                weight: 199,
+                description: "Лучшие суши на свете",
+            },
+        ],
+        [
+            {
+                id: 12,
+                name: "Суши 4",
+                image: testImage,
+                price: 300,
+                discount: 1,
+                weight: 199,
+                description: "Лучшие суши на свете",
+            },
+        ],
+        [
+            {
+                id: 12,
+                name: "Суши 4",
+                image: testImage,
+                price: 300,
+                discount: 1,
+                weight: 199,
+                description: "Лучшие суши на свете",
+            },
+        ],
+        [
+            {
+                id: 12,
+                name: "Суши 4",
+                image: testImage,
+                price: 300,
+                discount: 1,
+                weight: 199,
+                description: "Лучшие суши на свете",
+            },
+        ],
+        [
+            {
+                id: 12,
+                name: "Суши 4",
+                image: testImage,
+                price: 300,
+                discount: 1,
+                weight: 199,
+                description: "Лучшие суши на свете",
+            },
+        ],
+        [
+            {
+                id: 12,
+                name: "Суши 4",
+                image: testImage,
+                price: 300,
+                discount: 1,
+                weight: 199,
+                description: "Лучшие суши на свете",
+            },
+        ],
+        [
+            {
+                id: 12,
+                name: "Суши 4",
+                image: testImage,
+                price: 300,
+                discount: 1,
+                weight: 199,
+                description: "Лучшие суши на свете",
+            },
+        ],
+        [
+            {
+                id: 12,
+                name: "Суши 4",
+                image: testImage,
+                price: 300,
+                discount: 1,
+                weight: 199,
+                description: "Лучшие суши на свете",
+            },
+        ],
+        [
+            {
+                id: 12,
                 name: "Суши 4",
                 image: testImage,
                 price: 300,
@@ -194,7 +374,43 @@ export const Basket = () => {
             },
         ],
     ]);
+
+    const [basketSumm, setBasketSumm] = React.useState<number>(0);
+    const [basketCount, setBasketCount] = React.useState<number>(0);
     const [resetCount, setResetCount] = React.useState({});
+
+    React.useEffect(() => {
+        let summ = 0;
+        let count = 0;
+        if (basketData.length > 0) {
+            basketData.map((arrs: Product[]) => {
+                arrs.map((elem) => (summ += elem.price));
+                count += arrs.length;
+                setBasketCount(count);
+                setBasketSumm(summ);
+            });
+        } else {
+            setBasketSumm(0);
+            setBasketCount(0);
+        }
+    }, [basketData]);
+
+    const [basketOn, setBasketOn] = React.useState<boolean>(false);
+    const [contentOn, setContentOn] = React.useState<boolean>(false);
+
+    const handleClickPayment = () => {
+        let timesOut: any;
+        if (contentOn) {
+            timesOut = setTimeout(() => {
+                setContentOn((prev) => !prev);
+            }, 1000);
+        } else {
+            setContentOn(true);
+        }
+
+        setBasketOn((prev) => !prev);
+        () => clearTimeout(timesOut);
+    };
 
     return (
         <div className={styles.root}>
@@ -206,6 +422,58 @@ export const Basket = () => {
                     setData={setBasketData}
                     fullCard
                 />
+                <div
+                    className={
+                        styles.top_ele +
+                        " " +
+                        (basketOn ? styles.basket_active : "")
+                    }
+                >
+                    <div
+                        className={
+                            styles.icon_btn +
+                            " " +
+                            (basketOn ? styles.icon_btn_active : "")
+                        }
+                    >
+                        <img
+                            onClick={handleClickPayment}
+                            src={iconArrowTop}
+                            alt=""
+                        />
+                    </div>
+
+                    <div
+                        className={
+                            styles.content +
+                            " " +
+                            (basketOn ? styles.content_active : "")
+                        }
+                        style={{ display: contentOn ? "block" : "none" }}
+                    >
+                        How to blur background image using CSS ? - GeeksforGeeks
+                        geeksforgeeks.org›how…blur-background-image…css/ Меню
+                        CSS allows for easy customization of a web page’s color,
+                        font, spacing, and other elements to make it a crucial
+                        tool for creating professional-looking and functional
+                        websites. To blur a background image using CSS, you can
+                        use the filter property. Читать ещёCSS allows for easy
+                        customization of a web page’s color, font, spacing, and
+                        other elements to make it a crucial tool for creating
+                        professional-looking and functional websites. To blur a
+                        background image using CSS, you can use the filter
+                        property. The filter property is used to set the visual
+                        effect of an element. Syntax Скрыть Не найдено: размытый
+                    </div>
+
+                    <div className={styles.head}>
+                        <div className={styles.summ}>Сумма: {basketSumm}₽</div>
+                        <div className={styles.count}>
+                            Количество: {basketCount}
+                        </div>
+                        <button onClick={handleClickPayment}>Оплатить</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
